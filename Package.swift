40e40d64a9ff7b11d178283154601b5b4e8c0621
Yuri Karabatov <yuri@yurikarabatov.com>
2016-01-03 02:10:18 +0300

    Import func from stockfighter-lib module.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swiftfighter"
+    name: "Swiftfighter",
+    dependencies: [
+        .Package(url: "https://github.com/karabatov/stockfighter-lib.git", versions: Version(0,0,0)..<Version(1,0,0))
+    ]
 )
 
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,13 @@
+import Stockfighter
+
 print("∴ Swiftfighter ∵")
+
+var stop = false
+
+Heartbeat.isAPIup { ok in
+    print("API is \(ok ? "up :)" : "down :(")")
+    stop = true
+}
+
+repeat {} while !stop
 

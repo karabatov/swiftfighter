04f38f3f887f681d8e9b334f496eb894ac47f3ac
Yuri Karabatov <yuri@yurikarabatov.com>
2016-01-04 23:53:58 +0300

    Move to new library version with NSURLSession, base API URL and preliminary API key support.

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,7 +5,9 @@ print("∴ Swiftfighter ∵")
 
 var globalStop = false
 
-isStockfighterAPIup { heartbeat in
+let SF = SFAPI(baseAPI: "https://api.stockfighter.io/ob/api/", APIKey: "ho ho ho")
+
+SF.isAPIUp { heartbeat in
     if heartbeat.ok {
         print("API is up!")
     } else {

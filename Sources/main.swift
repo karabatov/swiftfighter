9a8d5e6b0e9fc8f57d7ee5f3f2a901d6478aafe6
Yuri Karabatov <yuri@yurikarabatov.com>
2016-01-08 22:11:20 +0300

    Support loading API key from file.

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,9 +3,32 @@ import Stockfighter
 
 print("∴ Swiftfighter ∵")
 
+/// Global stop flag, program is running only while it's false.
 var globalStop = false
+/// API key string, read from file during program start.
+var apiKey = ""
 
-let SF = SFAPI(baseAPI: "https://api.stockfighter.io/ob/api/", APIKey: "ho ho ho")
+// MARK: Command line arguments
+
+switch Process.arguments.count {
+case 1:
+    print("Usage:")
+    print("$ Swiftfighter api_key.txt")
+    fatalError()
+case 2:
+    if let
+        fileData = NSFileManager.defaultManager().contentsAtPath(Process.arguments[1]),
+        apiKeyStr = String(data: fileData, encoding: NSUTF8StringEncoding)
+    {
+        apiKey = apiKeyStr
+    } else {
+        fatalError()
+    }
+default:
+    break
+}
+
+let SF = SFAPI(baseAPI: "https://api.stockfighter.io/ob/api/", APIKey: apiKey)
 
 SF.isAPIUp { heartbeat in
     if heartbeat.ok {
